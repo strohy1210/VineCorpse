e179f106174c56b74153aafb9551f1620463a044
strohy120 <sarah.stroh@flatironschool.com>
2014-11-20 23:13:56 -0500

    added style

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,6 @@
 class SearchController < ApplicationController
   def search
-    # binding.pry
-<<<<<<< HEAD
-
-=======
-#if connected to internet/
->>>>>>> master
+ 
     vines = Vine.find_by_tag(params[:tag])
     @tag = params[:tag]
     @vines = vines[0..9]
