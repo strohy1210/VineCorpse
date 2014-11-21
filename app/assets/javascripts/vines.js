2fce7e342f0cd1810b3d57ba1891b2e4080a8d64
strohy120 <sarah.stroh@flatironschool.com>
2014-11-21 15:24:00 -0500

    made the edit page a list view

--- app/assets/javascripts/vines.js
+++ app/assets/javascripts/vines.js
@@ -2,24 +2,35 @@ $(function(){
 
 // if there's a class reel that exists on the page
 // with something in it
-if($('.reel').length > 0) {
 
-var allVideos = document.getElementsByTagName("video");
- 
- $("video").hide();
- $(allVideos[0]).show();
- allVideos[0].play();
- for(var i = 0; i < allVideos.length - 1; i++){
-   allVideos[i].onended = (function (a) {
-      return function() {         
-       $(allVideos[a]).hide()
-       $(allVideos[a + 1]).show()
-       allVideos[a + 1].play() 
+var $vidForMovie = $(".reel");
+ if ($vidForMovie && $vidForMovie.length > 0) {
+    $vidForMovie.hide();
+    $vidForMovie.eq(0).show();
+    $vidForMovie[0].play();
+
+     for(var i = 0; i < $vidForMovie.length - 1; i++){
+       $vidForMovie[i].onended = (function (a) {
+          return function() {         
+           $vidForMovie.eq(a).hide()
+           $vidForMovie.eq(a + 1).show()
+           $vidForMovie[a + 1].play() 
+         }
+       })(i);
      }
-   })(i);
- }
-
 }
 
-});
+var videosList = document.getElementsByClassName("clips")
+if (videosList && videosList.length>0) {
+videosList[0].play();
+ for(var i = 0; i < videosList.length - 1; i++){
+   videosList[i].onended = (function (a) {
+      return function() {         
+       videosList[a + 1].play() 
+     }
+   })(i);
+
+}
+}
+})
  
