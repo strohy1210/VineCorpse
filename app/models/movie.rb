853ab6ac4eacfb77722edfab379cfcee75112737
Fabio Kenji <seibzhen@gmail.com>
2014-11-23 18:36:44 -0500

    set default movie title if empty

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,12 @@
 class Movie < ActiveRecord::Base
   has_many :vines
   accepts_nested_attributes_for :vines
+
+  before_save :set_default_title_on_empty
+
+  private
+  def set_default_title_on_empty
+    self.title = 'Untitled' if self.title.empty?
+  end
   
 end
