71ea075655ba3f120bfc1594b40b182b49badeb6
Fabio Kenji <seibzhen@gmail.com>
2014-11-25 19:40:41 -0500

    implemented edit page

---
+++ app/assets/javascripts/movies.js
@@ -0,0 +1,6 @@
+$(function(){
+  $("#add-more-vines").click(function() {
+    $("#add-more-vines-form").show();
+    $("#add-more-vines-form").get(0).scrollIntoView();
+  });
+});
--- app/assets/javascripts/vines.js
+++ app/assets/javascripts/vines.js
@@ -9,17 +9,4 @@ if($('.reel').length > 0) {
   movieReel.start()
 }
 
-
-var videosList = document.getElementsByClassName("clips")
-if (videosList && videosList.length>0) {
-videosList[0].play();
- for(var i = 0; i < videosList.length - 1; i++){
-   videosList[i].onended = (function (a) {
-      return function() {         
-       videosList[a + 1].play() 
-     }
-   })(i);
-
-}
-}
-})
+});
