ab1878d2f34b711fbbdea565658bf2ca5688f421
Fabio Kenji <seibzhen@gmail.com>
2014-11-26 09:44:06 -0500

    fixing showing videos when movie doesnt have any vines

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,11 +2,13 @@ $(function () {
   if ($('.my-movies').length > 0) {
     var $movies = $('.my-movies').children().find('.reel-test');
     for (var i = 0; i < $movies.length; i++) {
-      var movieReel = new VineMovie({
-        source: $movies.eq(i)
-      });
-      movieReel.build();
-      movieReel.start();
-    }
+      if($movies.eq(i).children().length > 0){
+        var movieReel = new VineMovie({
+            source: $movies.eq(i)
+          });
+          movieReel.build();
+          movieReel.start();
+        }
+      }
   }
 });
